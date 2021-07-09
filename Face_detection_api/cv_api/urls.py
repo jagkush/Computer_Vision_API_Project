a23c9a901fe07cc1adc3f8f624bba47da28a1dda
Gbenga Kusade <jagkush2006@gmail.com>
2021-07-09 12:12:53 +0100

    Update urls.py

--- Face_detection_api/cv_api/urls.py
+++ Face_detection_api/cv_api/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from face_detector import views
-# from django.urls import patterns, include, url
+
 
 
 urlpatterns = [
